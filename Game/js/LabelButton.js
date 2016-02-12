8544818cd510447652fdd61611d16b537933f2d2
HawpT <kshaupty@gmail.com>
2016-02-11 17:17:25 -0700

    modified:   Game/HTML/base.html 	modified:   Game/js/LabelButton.js 	modified:   Game/js/PlayState.js

--- Game/js/LabelButton.js
+++ Game/js/LabelButton.js
@@ -1,6 +1,11 @@
+/*
+ * Label button is a wrapper class for Phaser Buttons so that the buttons can have
+ * text labels on them
+ */
+
 var LabelButton = function(game, x, y, key, label, callback, callbackContext, overFrame, outFrame, downFrame, upFrame){
     Phaser.Button.call(this, game, x, y, key, callback, callbackContext, overFrame, outFrame, downFrame, upFrame);
-    this.style = {'font': '10px Arial','fill': 'black'};
+    this.style = {'font': '32px Arial','fill': 'black'};
     this.anchor.setTo( 0.5, 0.5 );
     this.label = new Phaser.Text(game, 0, 0, label, this.style);
     this.label.anchor.setTo( 0.5, 0.5 );
--- Game/js/PlayState.js
+++ Game/js/PlayState.js
@@ -1,9 +1,10 @@
+//clas variables
 var soccer = soccer || {};
 var answer;
-var answerOutput;
-var button;
+var answerOutput, validate, problem;
 var zerobutton,onebutton,twobutton,threebutton,fourbutton,fivebutton,sixbutton,sevenbutton,eightbutton,ninebutton;
 var numbuttons;
+var delbutton, subbutton, button;
 var text = new Text();
 
 
@@ -27,8 +28,16 @@ soccer.PlayState.prototype = {
         eightbutton = new LabelButton(soccer.game,450,570,'numbutton','8',this.actionOnClicked,this,0,0,0,0);
         ninebutton = new LabelButton(soccer.game,510,570,'numbutton','9',this.actionOnClicked,this,0,0,0,0);
         zerobutton = new LabelButton(soccer.game,570,570,'numbutton','0',this.actionOnClicked,this,0,0,0,0);
-        numbuttons = this.game.add.group();
+        delbutton = new LabelButton(soccer.game,630,570,'numbutton','Delete',this.actionOnClicked,this,0,0,0,0);
+        subbutton = new LabelButton(soccer.game,400,300,'ball','SUBMIT',this.actionOnClicked,this,0,0,0,0);
+        subbutton.anchor.set(0.5);
+        subbutton.scale.setTo(0.5,0.5);
+        subbutton.style = {font: '70px Arial', fill: '#000'};
+        var subtext =
+        subbutton.setLabel('SUBMIT');
+        subbutton.onInputUp.add(this.checkAnswer.bind(this),this);
 
+        numbuttons = this.game.add.group();
         numbuttons.add(onebutton);
         numbuttons.add(twobutton);
         numbuttons.add(threebutton);
@@ -39,6 +48,7 @@ soccer.PlayState.prototype = {
         numbuttons.add(eightbutton);
         numbuttons.add(ninebutton);
         numbuttons.add(zerobutton);
+
         onebutton.onInputUp.add(this.clicked.bind(this, onebutton),this);
         twobutton.onInputUp.add(this.clicked.bind(this, twobutton),this);
         threebutton.onInputUp.add(this.clicked.bind(this, threebutton),this);
@@ -49,15 +59,17 @@ soccer.PlayState.prototype = {
         eightbutton.onInputUp.add(this.clicked.bind(this, eightbutton),this);
         ninebutton.onInputUp.add(this.clicked.bind(this, ninebutton),this);
         zerobutton.onInputUp.add(this.clicked.bind(this, zerobutton),this);
+        delbutton.onInputUp.add(this.clicked.bind(this, delbutton),this);
 
 
-
-
-        text = num1 + ' + ' + num2 + ' = ';
-        this.game.add.text(300, 284, text, {font: '32px Arial', fill: '#000'});
 
         answer = "";
-        answerOutput = this.game.add.text(400,500,answer,{font: '32px Arial', fill: '#000'});;
+        text = num1 + ' + ' + num2 + ' = ';
+        problem = this.game.add.text(290, 420, text, {font: '32px Arial', fill: '#000'});
+
+        validate = this.game.add.text(300,480,answer, {font: '32px Arial', fill: '#000'});
+
+        answerOutput = this.game.add.text(410,420,answer,{font: '32px Arial', fill: '#000'});
     },
 
     update: function () {
@@ -70,49 +82,63 @@ soccer.PlayState.prototype = {
     },
 
     clicked: function(b) {
-        switch(b) {
-            case onebutton:
-                answer += '1';
-                break;
-            case twobutton:
-                answer += '2';
-                break;
-            case threebutton:
-                answer += '3';
-                break;
-            case fourbutton:
-                answer += '4';
-                break;
-            case fivebutton:
-                answer += '5';
-                break;
-            case sixbutton:
-                answer += '6';
-                break;
-            case sevenbutton:
-                answer += '7';
-                break;
-            case eightbutton:
-                answer += '8';
-                break;
-            case ninebutton:
-                answer += '9';
-                break;
-            case zerobutton:
-                answer += '0';
-                break;
-            default:
-                answer += 'x';
 
-        }
-        answerOutput.setText(answer);
+            switch (b) {
+                case onebutton:
+                    answer += '1';
+                    break;
+                case twobutton:
+                    answer += '2';
+                    break;
+                case threebutton:
+                    answer += '3';
+                    break;
+                case fourbutton:
+                    answer += '4';
+                    break;
+                case fivebutton:
+                    answer += '5';
+                    break;
+                case sixbutton:
+                    answer += '6';
+                    break;
+                case sevenbutton:
+                    answer += '7';
+                    break;
+                case eightbutton:
+                    answer += '8';
+                    break;
+                case ninebutton:
+                    answer += '9';
+                    break;
+                case zerobutton:
+                    answer += '0';
+                    break;
+                case delbutton:
+                    answer = answer.substring(0, answer.length - 1);
+                    break;
+                default:
+                    throw "clicked() did not receive a button in the parameter";
+                    answer += '';
+
+            }
+            answerOutput.setText(answer);
+
 
     },
 
-    backspaceAnswer: function(){
-        if (answer.length > 0)
-            answer = answer.substring(0,answer.length);
+    checkAnswer: function() {
+        if (num1 + num2 == parseInt(answer)) {
+            validate.setText("RIGHT!");
+        }
+        else {
+            validate.setText("WRONG!");
+        }
+
+        answer = "";
+        answerOutput.setText(answer);
     }
+
 
 };
 
