373810cef50207f8be5cd3b3c86f64ee76d79e6f
colettebedoya <colettebedoya@gmail.com>
2016-03-25 13:14:07 -0600

    new file:   playgame/static/js/Preload.js

---
+++ playgame/static/js/Preload.js
@@ -0,0 +1,28 @@
+
+var soccer = soccer || {};
+
+soccer.Preload = function() {};
+
+soccer.Preload.prototype = {
+    preload: function() {
+	
+        //load images
+	    this.load.image('ball','../static/playgame/ball.png');
+        this.load.image('field','../static/playgame/soccerfield.jpg');
+        this.load.spritesheet('player','../static/playgame/spineboy-old-walk.png',105,170,27);
+<<<<<<< HEAD
+        this.load.spritesheet('opponent','../static/playgame/opponent-walk.png',105,170,27);
+=======
+>>>>>>> 19eded62324a9f20d6f3f2768dfaa6692aadc563
+        this.load.image('numbutton','../static/playgame/orangebutton.png');
+        this.load.image('playfield','../static/playgame/playfield.png');
+
+
+    },
+
+    //immediately when preload is done
+    create: function() {
+        //call the Main Menu state
+        this.state.start('MainMenu');
+    }
+};
