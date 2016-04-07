3280adb144091b19c7b5cb0dd2755732f34f3186
HawpT <kshaupty@gmail.com>
2016-04-07 16:52:24 -0600

    new file:   playgame/static/js/Teaching.js

---
+++ playgame/static/js/Teaching.js
@@ -0,0 +1,19 @@
+
+var soccer = soccer || {};
+
+soccer.Teaching = function() {};
+
+soccer.Teaching.prototype = {
+    preload: function() {
+    },
+
+    //immediately when preload is done
+    create: function() {
+        //add stuff here!!!
+    },
+
+    update: function(){
+
+    }
+
+};
