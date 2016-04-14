4e1c3fe08d4574f6938bc754b4aab17105dd982f
colettebedoya <colettebedoya@gmail.com>
2016-04-14 16:28:51 -0600

    modified:   playgame/static/js/Teaching.js

--- playgame/static/js/Teaching.js
+++ playgame/static/js/Teaching.js
@@ -2,18 +2,27 @@ var soccer = soccer || {};
 
 soccer.Teaching = function() {};
 var background, problem, text, move, ballgroup, xballgroup, aswer;
+var playGameButton;
 //prototype the Teaching state
 soccer.Teaching.prototype = {
     create: function () {
-
+		
+		
+		
     	background = this.game.add.image(0,0,'field');
+    	//playGameButton = this.game.add.button(600, 50, 'lgbutton', "PLAY GAME",this.clicked, this);
+    	playGameButton = new LabelButton
+    	        (soccer.game,600,50,'lgbutton','PLAY GAME',this.actionOnClicked,this,0,0,0,0);
+    	playGameButton.onInputUp.add(this.playScreen.bind(this));        
     	//move = 100;
     	answer = operand1+operand2;
     	text = operand1 + ' + ' + operand2 + ' = '+ answer;
-		probelm=this.game.add.text(350, operand1, text, {font: '64px Arial', fill: '#000'});
+		probelm=this.game.add.text(150, operand1, text, {font: '64px Arial', fill: '#000'});
 		ballgroup = this.game.add.group();
 		ballgroup.scale.set(.2);
-
+		
+		
+//level one addition
 		if(operand1<10){
 			for(var i=0; i<operand1; i++){
 				ballgroup.create(300*i, 350, 'ball');
@@ -42,7 +51,9 @@ soccer.Teaching.prototype = {
     },
 
     //when the button is clicked, move to the Play State
-    clicked: function () {
-        
-    }
+    
+    playScreen: function() {
+        this.game.state.start('PlayState');
+    },
+    
 }
